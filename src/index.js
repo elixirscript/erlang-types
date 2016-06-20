f5063be62db4fde840b628b8622a5d186b6f58b9
Bryan Joseph <bryanjos@gmail.com>
2016-06-20 18:36:00 -0500

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Tuple from './erlang-types/tuple.js';
+import PID from './erlang-types/pid.js';
+import Reference from './erlang-types/reference.js';
+import BitString from './erlang-types/bit_string.js';
+
+
+export default {
+  Tuple,
+  PID,
+  Reference,
+  BitString
+};
